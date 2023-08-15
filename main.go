1c0a0e6d7c727e0ebbb904f26d1ddaf06f12e002
Sigit Priadi <18523125@students.uii.ac.id>
2023-08-15 10:50:38 +0700

    add configuration

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"time"
 )
 
 type FileServer struct {
@@ -37,6 +40,39 @@ func (fs *FileServer) readLoop(conn net.Conn) {
 	}
 }
 
-func main() {
+func sendFile(size int) error {
+	file := make([]byte, size)
+	_, err := io.ReadFull(rand.Reader, file)
+	if err != nil {
+		return err
+	}
 
+	conn, err := net.Dial("tcp", "127.0.0.1:3000") // Specify the port number
+	if err != nil {
+		return err
+	}
+	defer conn.Close() // Close the connection when done
+
+	n, err := conn.Write(file)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("written %d bytes over the network\n", n)
+	return nil
+}
+
+func main() {
+	fs := &FileServer{}
+	go fs.start()
+
+	time.Sleep(1 * time.Second) // Sleep briefly to allow the server to start
+
+	go func() {
+		time.Sleep(4 * time.Second)
+		sendFile(4000)
+	}()
+
+	// Sleep to allow time for the file to be sent before the program exits
+	time.Sleep(10 * time.Second)
 }
